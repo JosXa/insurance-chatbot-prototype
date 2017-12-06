35919bf35a95a5ca4e7220cbe893ff8c69fd53d4
JosXa <joscha.goetzer@gmail.com>
2017-12-06 18:24:08 +0100

    Deployment

--- clients/telegram.py
+++ clients/telegram.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+from threading import Thread
 
 from flask import logging, Flask, request
 from telegram import Bot
@@ -19,13 +20,16 @@ class TelegramClient(IBotAPIClient):
         self.updater = None  # type: Updater
         self.bot = None  # type: Bot
         self._running = False
+        self.__threads = list()
+
+    def _init_thread(self, target, *args, **kwargs):
+        thr = Thread(target=target, args=args, kwargs=kwargs)
+        thr.start()
+        self.__threads.append(thr)
 
     def initialize(self):
         self.updater = Updater(token=self._token)
         self.bot = self.updater.bot
-
-    def add_plaintext_handler(self, callback: Callable):
-        self.updater.dispatcher.add_handler(MessageHandler(Filters.text, callback))
 
     def _webhook_endpoint(self):
         data = request.get_data(as_text=True)
@@ -33,7 +37,11 @@ class TelegramClient(IBotAPIClient):
         self.updater.update_queue.put(data)
 
     def start_listening(self):
+        # Start ptb threads
         self.updater.job_queue.start()
+        self._init_thread(self.updater.dispatcher.start)
+
+        # Construct URL and set webhook
         url = self._webhook_url + self._token
         self.bot.set_webhook(url)
         self._app.add_url_rule(f"/{self._token}",
@@ -41,10 +49,12 @@ class TelegramClient(IBotAPIClient):
                                methods=['POST', 'GET'])
 
     def stop_listening(self):
+        self.updater.job_queue.stop()
+        self.updater.dispatcher.stop()
         self.updater.stop()
 
-    def connect(self):
-        pass
+    def add_plaintext_handler(self, callback: Callable):
+        self.updater.dispatcher.add_handler(MessageHandler(Filters.text, callback))
 
     def send_message(self, recipient_id, text):
         self.bot.send_message(recipient_id, text)
