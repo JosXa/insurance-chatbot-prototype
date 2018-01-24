42f0e8e3fd4dd5c0182d38fa949a581a9c9aeb93
JosXa <joscha.goetzer@gmail.com>
2018-01-24 13:19:43 +0100

    Deployment

--- appglobals.py
+++ appglobals.py
@@ -11,7 +11,7 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 def db():
     global _db
     if not _db:
-        _db = connect(settings.DATABASE_URI)
+        _db = connect(settings.DATABASE_URL)
     return _db
 
 
