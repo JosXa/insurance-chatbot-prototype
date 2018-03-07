a5fba012adc11b7bc89ae6c5bc0fb64b35177593
JosXa <joscha.goetzer@gmail.com>
2018-03-07 21:46:04 +0100

    Deployment

--- clients/facebook.py
+++ clients/facebook.py
@@ -170,82 +170,16 @@ class FacebookClient(IBotAPIClient):
             # return msg
 
     def show_typing(self, user_id):
-        self._page.typing_on(user_id)
+        try:
+            self._page.typing_on(user_id)
+        except:
+            log.error("Turning typing indication on failed.")
 
     def end_typing(self, user_id):
-        self._page.typing_off(user_id)
+        try:
+            self._page.typing_off(user_id)
+        except:
+            log.error("Turning typing indication off failed.")
 
     def add_error_handler(self, callback):
         self._error_handler = callback
-
-
-"""
-import logging
-import random
-
-from decouple import config
-from fbmq import QuickReply, Event
-from fbpage import page
-from flask import request, Flask
-
-import settings
-from model import User
-# parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# os.sys.path.insert(0, parentdir)
-log = logging.getLogger(__name__)
-
-app = Flask(__name__)
-
-page.show_starting_button("START_BOT")
-
-
-@app.route('/', methods=['POST'])
-def webhook():
-    page.handle_webhook(request.get_data(as_text=True))
-    return "ok"
-
-
-@page.handle_delivery
-def delivery(event: Event):
-    pass
-
-
-# @page.handle_referral
-# def referral(event: Event):
-#     user = User.from_event(event)
-#     user.referral = event.referral_ref
-#     user.save()
-#     start(None, event)
-
-
-@page.callback(['START_BOT'])
-def start_handler(payload, event):
-    user = User.from_event(event)
-
-    quick_replies = [
-        QuickReply(title="Yeah, sure", payload="yes-more-info"),
-    ]
-
-    page.send(user.sender_id,
-              "STARTED",
-              quick_replies=quick_replies)
-
-
-@page.handle_message
-def message_handler(event: Event):
-    query = event.message_text.lower()
-
-    if query == 'start':
-        return start(None, event)
-    elif query == 'love':
-        page.send(1433118220058887, random.choice(loveyous))
-        return
-
-        # start(None, event)
-
-def start():
-    log.info("Listening...")
-    app.run(host=config('WEBHOOK_URL'), port=config('WEBHOOK_PORT'), ssl_context=(
-        '/home/joscha/cert/josxa.jumpingcrab.com/fullchain.pem',
-        '/home/joscha/cert/josxa.jumpingcrab.com/privkey.pem'))
-"""
